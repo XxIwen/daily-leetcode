f36f64c53b61fa56c4d79e81e6643257c001d3a9
xavier-9527 <bai_cuit@163.com>
2022-03-01 18:04:37 +0800

    feat(jz-84): add the solution 1

--- edit-code.js
+++ edit-code.js
@@ -1,8 +1,53 @@
 /*
- * @Description:
- * @Author: Kevin
- * @Email: baishi.wang@ncs.com.sg
- * @Date: 2022-03-01 17:25:15
- * @LastEditTime: 2022-03-01 17:26:09
- * @LastEditors: Kevin
+ * function TreeNode(x) {
+ *   this.val = x;
+ *   this.left = null;
+ *   this.right = null;
+ * }
  */
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param root TreeNode类
+ * @param sum int整型
+ * @return int整型
+ */
+function FindPath(root, sum) {
+  // write code here
+  var result = [];
+  if (root) {
+    calculatePath(root, sum, result);
+  }
+  return result.length;
+}
+
+function calculatePath(node, sum, result) {
+  path(node, 0, sum, result);
+  if (node.left) {
+    calculatePath(node.left, sum, result);
+  }
+  if (node.right) {
+    calculatePath(node.right, sum, result);
+  }
+}
+
+function path(node, total, sum, result) {
+  total += node.val;
+
+  if (total === sum) {
+    result.push(total);
+  }
+
+  if (node.left) {
+    path(node.left, total, sum, result);
+  }
+
+  if (node.right) {
+    path(node.right, total, sum, result);
+  }
+}
+
+module.exports = {
+  FindPath: FindPath,
+};
