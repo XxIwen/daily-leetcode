f5148d6652872ef6980153856d73f274c0d9f52b
xavier-9527 <bai_cuit@163.com>
2022-03-02 00:37:15 +0800

    feat(jz-26): add the question

--- edit-code.js
+++ edit-code.js
@@ -1,29 +1,12 @@
-/*function TreeLinkNode(x){
+/* function TreeNode(x) {
     this.val = x;
     this.left = null;
     this.right = null;
-    this.next = null;
-}*/
-function GetNext(pNode) {
-  // write code here
-  if (!pNode) return null;
-
-  if (pNode.right) {
-    pNode = pNode.right;
-    while (pNode.left) {
-      pNode = pNode.left;
-    }
-    return pNode;
-  } else {
-    while (pNode) {
-      if (!pNode.next) {
-      } else if (pNode == pNode.next.left) {
-        return pNode.next;  // 当前节点是父节点左孩子，那么当前节点的父节点就是下一个节点
-      }
-      pNode = pNode.next;
-    }
-  }
+} */
+function HasSubtree(pRoot1, pRoot2)
+{
+    // write code here
 }
 module.exports = {
-  GetNext: GetNext,
+    HasSubtree : HasSubtree
 };
