f1726f200f4ebc7e9dc67d1a96e9198b1f305427
xavier-9527 <bai_cuit@163.com>
2022-03-03 21:52:26 +0800

    feat(leetcode-solution): add the solution of 455

--- edit-code.js
+++ edit-code.js
@@ -3,6 +3,19 @@
  * @param {number[]} s
  * @return {number}
  */
- var findContentChildren = function(g, s) {
+var findContentChildren = function (g, s) {
+  var g = g.sort((a, b) => a - b);
+  var s = s.sort((a, b) => a - b);
+  var sum = 0;
+  var cookie = 0;
+  var child = 0;
+  while (cookie < s.length && child < g.length) {
+    if (s[cookie] >= g[child]) {
+      sum++;
+      child++;
+    }
+    cookie++;
+  }
 
+  return sum;
 };
