2755a6c673112f7acdd5bcd89bc5f0c857865a6e
xavier-9527 <bai_cuit@163.com>
2022-03-05 01:03:39 +0800

    feat(leetcode-solution): add the solution of 322

--- edit-code.js
+++ edit-code.js
@@ -1,45 +1,20 @@
 /**
- * @param {number} amount
  * @param {number[]} coins
+ * @param {number} amount
  * @return {number}
  */
-// 下面这个用例调试通过，但是在leetcode上通不过
-// var coins = [2];
-// var amount = 3;
-
-// var coins = [1,2,5];
-// var amount = 11;
-
-// 超出时间限制
-// var coins = [411,412,413,414,415,416,417,418,419,420,421,422];
-// var amount = 9864;
-
-var result = Infinity;
 var coinChange = function (coins, amount) {
-  if (amount == 0) {
-    return 0;
+  if (!amount) return 0;
+  var dp = new Array(amount + 1).fill(Infinity);
+
+  dp[0] = 0;
+  for (var j = 0; j <= amount; j++) {
+    for (var i = 0; i < coins.length; i++) {
+      if (j >= coins[i]) {
+        dp[j] = Math.min(dp[j], dp[j-coins[i]] + 1);
+      }
+    }
   }
-  if (coins.legnth === 1 && coins[0] === 2) return -1; // 针对第一个用例的
-  coins = coins.sort((a, b) => a - b);
-  core(coins, amount, coins.length - 1, 0);
-  // console.log(result);
-  // console.log(result == Infinity ? -1 : result);
-  return result == Infinity ? -1 : result;
+
+  return dp[amount] == Infinity ? -1 : dp[amount];
 };
-
-function core(coins, amount, index, count) {
-  if (amount == 0) {
-    // console.log({amount, result, count});
-    result = Math.min(count, result);
-    return;
-  }
-
-  if (index < 0) {
-    return;
-  }
-  for (var i = Math.floor(amount / coins[index]); i >= 0 && i + count < result; i--) {
-    core(coins, amount - (i * coins[index]), index - 1, count + i);
-  }
-}
-
-// coinChange(coins, amount);
