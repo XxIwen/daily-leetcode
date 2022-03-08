1cfaed8078c266bcc3e9acd335d3fd5d079ba0cd
xavier-9527 <bai_cuit@163.com>
2022-03-08 23:51:08 +0800

    feat(leetcode-pd): add the solution of 198

--- edit-code.js
+++ edit-code.js
@@ -1,16 +1,22 @@
 /**
- * @param {number} amount
- * @param {number[]} coins
+ * @param {number[]} nums
  * @return {number}
  */
-var change = function (amount, coins) {
-  var dp = new Array(amount + 1).fill(0);
-  dp[0] = 1;
-  for (var i = 0; i < coins.length; i++) {
-    for (var j = coins[i]; j <= amount; j++) {
-      dp[j] += dp[j - coins[i]];
-    }
+var rob = function (nums) {
+  if (nums.length == 0) {
+    return 0;
   }
 
-  return dp[amount];
+  if (nums.length == 1) {
+    return nums[0];
+  }
+
+  var dp = new Array(nums.length).fill(0);
+  dp[0] = nums[0];
+  dp[1] = Math.max(nums[0], nums[1]);
+  for (var i = 2; i < nums.length; i++) {
+    dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+  }
+
+  return dp[nums.length - 1];
 };
