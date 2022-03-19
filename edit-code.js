9d6cb5cb8df53d6f9399f52d60aaa9eedc22e3d3
xavier-9527 <bai_cuit@163.com>
2022-03-19 15:21:08 +0800

    feat(awesome-coding-js): add the practice of instanceof

--- edit-code.js
+++ edit-code.js
@@ -1,18 +1,13 @@
-function Parent() {
-  this.name = "fedaily";
+// a instanceof Object 判断Object的prototype是否在a的原型链上
+function myInstanceof(target, origin) {
+  const proto = target.__proto__;
+  if (proto) {
+    if (proto === origin.prototype) {
+      return true;
+    } else {
+      myInstanceof(proto, origin);
+    }
+  } else {
+    return false;
+  }
 }
-
-Parent.prototype.getName = function () {
-  return this.name;
-};
-
-Parent.prototype.a1 = 3;
-Parent.prototype.a2 = 4;
-
-
-var child1 = new Parent();
-var child2 = new Parent();
-child1.a1 = 1;
-child2.a2 = 2;
-console.log({'child1.a1': child1.a1, 'child1.a2': child1.a2});
-console.log({'child2.a1': child2.a1, 'child2.a2': child2.a2});
